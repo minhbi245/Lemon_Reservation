e92a6710997e523b1a14a221e76e0d4d159f9cd5
tiep.vv <tiep.vv@zinza.com.vn>
2023-06-13 14:43:16 +0700

    Initial project

---
+++ StartingProject/StartingProject/StartingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartingProjectApp.swift
+//  StartingProject
+//
+//  Created by macbook on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct StartingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
